7041a448be623d2e0377b4f2ce8603e54182ea70
sergipe085 <dapscreed@gmail.com>
2023-11-24 08:21:58 -0300

    level manager

--- Scenes/main/level_manager.gd
+++ Scenes/main/level_manager.gd
@@ -3,10 +3,14 @@ extends Node
 @export var menu_scene: Resource = null;
 @export var history_scene: Resource = null;
 @export var world_scene: Resource = null;
+@export var failed_scene: Resource = null;
+@export var completed_scene: Resource = null;
 
 var menu_instance: menu;
 var history_instance: history;
 var world_instance: world;
+var failed_instance: Node;
+var completed_instance: Node;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,3 +28,15 @@ func start_world():
 	history_instance.queue_free();
 	world_instance = load(world_scene.resource_path).instantiate();
 	add_child(world_instance);
+	world_instance.completed = start_completed;
+	world_instance.failed = start_failed;
+
+func start_completed():
+	world_instance.queue_free();
+	completed_instance = load(completed_scene.resource_path).instantiate();
+	add_child(completed_instance);
+	
+func start_failed():
+	completed_instance.queue_free();
+	failed_instance = load(failed_scene.resource_path).instantiate();
+	add_child(failed_instance);
--- Scenes/world.gd
+++ Scenes/world.gd
@@ -4,11 +4,21 @@ class_name world;
 
 @export var _mission_manager: mission_manager;
 
+var completed: Callable;
+var failed: Callable;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	_mission_manager.on_complete_all_missions = _completed
+	_mission_manager.on_failed = _failed;
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _completed():
+	completed.bind().call();
+	
+func _failed():
+	failed.bind().call()
